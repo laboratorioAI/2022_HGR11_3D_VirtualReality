32f9276956728a20f5b6b3c58ce86e93fb0ab17c
leonardovaldivieso <80180384+leonardovaldivieso@users.noreply.github.com>
2023-07-19 22:18:54 -0500

    Delete HGR_MODEL directory

--- HGR_MODEL/preprocessingSignal.m
+++
@@ -1,8 +0,0 @@
-function emg = preprocessingSignal(emg)
-    unknownGesture=zeros(1000,8);
-    unknownGesture = emg;
-    ordenFiltro=4;
-    freqFiltro=0.05;
-    [Fb, Fa] = butter(ordenFiltro, freqFiltro, 'low'); % creating filter
-    emg = filtfilt(Fb, Fa, abs(unknownGesture));
-end
